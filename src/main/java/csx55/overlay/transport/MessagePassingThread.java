54629a18ac23f4ee5bf0da08983514e2a8d2ecc2
Matt Young <honeybadgerofdoom@gmail.com>
2024-02-11 13:21:42 -0700

    clean up

--- src/main/java/csx55/overlay/transport/MessagePassingThread.java
+++ src/main/java/csx55/overlay/transport/MessagePassingThread.java
@@ -10,16 +10,20 @@ import java.util.*;
 public class MessagePassingThread implements Runnable {
 
     private final MessagingNode node;
+    private final int numberOfRounds;
 
-    public MessagePassingThread(MessagingNode node) {
+    public MessagePassingThread(MessagingNode node, int numberOfRounds) {
         this.node = node;
+        this.numberOfRounds = numberOfRounds;
     }
 
     @Override
     public void run() {
         Random rng = this.node.getRng();
         int numTasks = rng.nextInt(1001);
-        // Do stuff
+        for (int j = 0; j < this.numberOfRounds; j++) {
+            // do stuff
+        }
     }
     
 }
