8d4ac8580192a2bbc82b476914bdd79d5ffe94a5
Matt Young <honeybadgerofdoom@gmail.com>
2024-02-18 09:18:55 -0700

    updating TaskProcessor

--- src/main/java/csx55/overlay/transport/TaskProcessor.java
+++ src/main/java/csx55/overlay/transport/TaskProcessor.java
@@ -4,23 +4,18 @@ import csx55.overlay.node.MessagingNode;
 import csx55.overlay.node.PartnerNodeRef;
 import csx55.overlay.util.TaskManager;
 import csx55.overlay.wireformats.Event;
-import csx55.overlay.wireformats.LoadBalanced;
 import csx55.overlay.wireformats.TaskAverage;
 import csx55.overlay.wireformats.TaskDelivery;
-
-import java.util.*;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.List;
+import java.util.ArrayList;
 
 public class TaskProcessor implements Runnable {
 
     private MessagingNode node;
     private final int numberOfRounds;
-    private TaskManager taskManager;
-    private Queue<LoadBalanced> loadBalancedQueue = new ArrayDeque<>();
-    private Queue<TaskAverage> taskAverageQueue = new ArrayDeque<>();
-    private Queue<TaskDelivery> taskDeliveryQueue = new ArrayDeque<>();
-    private volatile boolean loadBalancedReceived;
-    private int iteration;
+    private List<TaskManager> taskManagerList;
+
 
     public TaskProcessor(MessagingNode node, int numberOfRounds) {
         this.node = node;
@@ -30,166 +25,90 @@ public class TaskProcessor implements Runnable {
     public void run() {
         ConcurrentLinkedQueue<Event> taskQueue = this.node.getThreadPool().getTaskQueue();
         PartnerNodeRef partnerNodeRef = this.node.getOneNeighbor();
-        for (this.iteration = 0; iteration < this.numberOfRounds; iteration++) {
 
-            System.out.println("Beginning iteration " + iteration);
-            loadBalancedReceived = false;
+        for (int i = 0; i < this.numberOfRounds; i++) {
+            TaskManager taskManager = new TaskManager(this.node.getRng());
+            this.taskManagerList.add(taskManager);
+        }
 
-            this.taskManager = new TaskManager(this.node.getRng());
+        for (int i = 0; i < this.numberOfRounds; i++) {
+
+            TaskManager taskManager = new TaskManager(this.node.getRng());
             
-            getTaskAverage(partnerNodeRef);
+            getTaskAverage(partnerNodeRef, i);
 
-            // FIXME Where do these 2 queue handlers go?
-            System.out.println("Relaying all " + this.taskAverageQueue.size() + " TaskAverage messages.");
-            while (!this.taskAverageQueue.isEmpty()) {
-                TaskAverage taskAverage = this.taskAverageQueue.poll();
-                handleTaskAverage(taskAverage);
-            }
+            balanceLoad(partnerNodeRef, i);
+
             
-            System.out.println("Relaying all " + this.taskDeliveryQueue.size() + " TaskDelivery messages.");
-            while (!this.taskDeliveryQueue.isEmpty()) {
-                TaskDelivery taskDelivery = this.taskDeliveryQueue.poll();
-                handleTaskDelivery(taskDelivery);
-            }
-
-            balanceLoad(partnerNodeRef);
-
-            System.out.println("Waiting until I'm load balanced...");
-            while (!taskManager.isLoadBalanced()) {
-                // Thread.onSpinWait();
-            }
-
-            System.out.println("Load is balanced, sending LoadBalanced message...");
-            LoadBalanced myLoadBalanced = new LoadBalanced(this.node.getId(), this.iteration);
-            partnerNodeRef.writeToSocket(myLoadBalanced);
-
-            // ToDo add correct # tasks to taskQueue
-
-            // ToDo wait for taskQueue to be empty
-//            try {
-//                Thread.sleep(1000);
-//            } catch (InterruptedException e) {
-//                System.out.println("Failed to sleep thread " + e);
-//            }
-
-            System.out.println("Relaying all " + this.loadBalancedQueue.size() + " LoadBalanced messages.");
-            while (!this.loadBalancedQueue.isEmpty()) {
-                LoadBalanced loadBalanced = this.loadBalancedQueue.poll();
-                relayLoadBalanced(loadBalanced);
-            }
-
-            System.out.println("All LoadBalanced messages relayed. Waiting for my own LoadBalanced message...");
-            while (!loadBalancedReceived) {
-                // Thread.onSpinWait();
-            }
-            System.out.println("Received my own LoadBalanced message");
-
             System.out.println(taskManager);
             
         }
     }
 
-    private void getTaskAverage(PartnerNodeRef partnerNodeRef) {
-        TaskAverage taskAverage = new TaskAverage(this.taskManager.getCurrentNumberOfTasks(), this.node.getId(), this.iteration);
+    private void getTaskAverage(PartnerNodeRef partnerNodeRef, int iteration) {
+        TaskManager taskManager = this.taskManagerList.get(iteration);
+        TaskAverage taskAverage = new TaskAverage(taskManager.getCurrentNumberOfTasks(), this.node.getId(), iteration);
         partnerNodeRef.writeToSocket(taskAverage);
     }
 
-    private void balanceLoad(PartnerNodeRef partnerNodeRef) {
-        while (!this.taskManager.averageIsSet()) {
+    private void balanceLoad(PartnerNodeRef partnerNodeRef, int iteration) {
+        TaskManager taskManager = this.taskManagerList.get(iteration);
+        while (!taskManager.averageIsSet()) {
             // Thread.onSpinWait();
         } // We can't hear anything from the registry during this time
-        if (this.taskManager.shouldGiveTasks()) {
-            TaskDelivery taskDelivery = new TaskDelivery(this.taskManager.getTaskDiff(), this.node.getId(), this.iteration);
-            this.taskManager.giveTasks(this.taskManager.getTaskDiff());
+        if (taskManager.shouldGiveTasks()) {
+            TaskDelivery taskDelivery = new TaskDelivery(taskManager.getTaskDiff(), this.node.getId(), iteration);
+            taskManager.giveTasks(taskManager.getTaskDiff());
             partnerNodeRef.writeToSocket(taskDelivery);
         }
     }
 
     public void handleTaskAverage(TaskAverage taskAverage) {
-        if (this.iteration <= taskAverage.getIteration()) {
-            if (taskAverage.nodeIsFirst(this.node.getId())) {
-                double average = (double) taskAverage.getSum() / taskAverage.getNumberOfNodes();
-                synchronized (this.taskManager) {
-                    this.taskManager.setAverage(average);
-                }
-            } else {
-                relayTaskAverage(taskAverage);
+        int iteration = taskAverage.getIteration();
+        TaskManager taskManager = this.taskManagerList.get(iteration);
+        if (taskAverage.nodeIsFirst(this.node.getId())) {
+            double average = (double) taskAverage.getSum() / taskAverage.getNumberOfNodes();
+            synchronized (taskManager) {
+                taskManager.setAverage(average);
             }
-        }
-        else {
-            this.taskAverageQueue.add(taskAverage);
+        } else {
+            relayTaskAverage(taskManager, taskAverage);
         }
     }
 
     public void handleTaskDelivery(TaskDelivery taskDelivery) {
-        while (!this.taskManager.averageIsSet()) {
+        int iteration = taskDelivery.getIteration();
+        TaskManager taskManager = this.taskManagerList.get(iteration);
+        while (!taskManager.averageIsSet()) {
             // Thread.onSpinWait();
         }
-        if (this.iteration <= taskDelivery.getIteration()) {
-            if (!taskDelivery.nodeIsFirst(this.node.getId())) {
-                this.taskManager.handleTaskDelivery(taskDelivery);
-                if (taskDelivery.getNumTasks() > 0) {
-                    relayTaskDelivery(taskDelivery);
-                }
-            }
-            else {
-                int tasksLeft = taskDelivery.getNumTasks();
-                this.taskManager.absorbExcessTasks(tasksLeft);
+        if (!taskDelivery.nodeIsFirst(this.node.getId())) {
+            taskManager.handleTaskDelivery(taskDelivery);
+            if (taskDelivery.getNumTasks() > 0) {
+                relayTaskDelivery(taskDelivery);
             }
         }
         else {
-            this.taskDeliveryQueue.add(taskDelivery);
+            int tasksLeft = taskDelivery.getNumTasks();
+            taskManager.absorbExcessTasks(tasksLeft);
         }
     }
 
-    public synchronized void handleLoadBalanced(LoadBalanced loadBalanced) {
-        /*
-        * FIXME
-        *  - This isn't working, check logic
-        * */
-        if (this.iteration != loadBalanced.getIteration()) {
-            this.loadBalancedQueue.add(loadBalanced);
-        }
-        else if (loadBalanced.nodeIsFirst(this.node.getId())) {
-            this.loadBalancedReceived = true;
-        }
-        else if (this.taskManager.isLoadBalanced()) {
-            relayLoadBalanced(loadBalanced);
-        }
-        else {
-            this.loadBalancedQueue.add(loadBalanced);
-        }
-    }
-
-    private void relayLoadBalanced(LoadBalanced loadBalanced) {
-        if (this.iteration != loadBalanced.getIteration()) {
-            this.loadBalancedQueue.add(loadBalanced);
-        }
-        String lastNode = loadBalanced.processRelay(this.node.getId());
-        for (String key : this.node.getPartnerNodes().keySet()) {
-            if (!key.equals(lastNode)) {
-                PartnerNodeRef relayTarget = this.node.getPartnerNodes().get(key);
-                relayTarget.writeToSocket(loadBalanced);
-            }
-        }
-    }
-
-    private void relayTaskAverage(TaskAverage taskAverage) {
-        String lastNode = taskAverage.processRelay(this.node.getId(), this.taskManager.getCurrentNumberOfTasks());
-        for (String key : this.node.getPartnerNodes().keySet()) {
-            if (!key.equals(lastNode)) {
-                PartnerNodeRef relayTarget = this.node.getPartnerNodes().get(key);
-                relayTarget.writeToSocket(taskAverage);
-            }
-        }
+    private void relayTaskAverage(TaskManager taskManager, TaskAverage taskAverage) {
+        String lastNode = taskAverage.processRelay(this.node.getId(), taskManager.getCurrentNumberOfTasks());
+        relay(lastNode, taskAverage);
     }
 
     private void relayTaskDelivery(TaskDelivery taskDelivery) {
         String lastNode = taskDelivery.processRelay(this.node.getId());
+        relay(lastNode, taskDelivery);
+    }
+
+    private void relay(String lastNode, Event event) {
         for (String key : this.node.getPartnerNodes().keySet()) {
             if (!key.equals(lastNode)) {
                 PartnerNodeRef relayTarget = this.node.getPartnerNodes().get(key);
-                relayTarget.writeToSocket(taskDelivery);
+                relayTarget.writeToSocket(event);
             }
         }
     }
